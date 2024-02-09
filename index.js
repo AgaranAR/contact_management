13ba25010c14943fed76b8c874a56c6f642fff67
Agaran A.R <139426185+AgaranAR@users.noreply.github.com>
2024-02-09 18:53:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const express = require('express')
-const PORT = 5000;
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
+
+connectDb();
 const app = express();
-app.use("/api/contacts", require("./routes/contactroutes"));
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
